0bcac2b4e77cba0302d3e70ab547d1e96ec4eb00
arufiand <alfiandannyarmanta@gmail.com>
2023-10-18 18:46:17 +0700

    feat(Skill): Updating Skill Data

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,6 @@
-import { CpuChipIcon } from '@heroicons/react/24/solid';
+import { CheckIcon, CpuChipIcon } from '@heroicons/react/24/solid';
 import React from 'react';
+import { skillsData } from '../../data/skills.js';
 
 const Skills = () => {
   return (
@@ -16,14 +17,14 @@ const Skills = () => {
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-          {/*{skills.map((skill) => (*/}
-          {/*  <div key={skill} className="p-2 sm:w-1/2 w-full">*/}
-          {/*    <div className="bg-gray-800 rounded flex p-4 h-full items-center">*/}
-          {/*      <CheckBadgeIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />*/}
-          {/*      <span className="title-font font-medium text-white">{skill}</span>*/}
-          {/*    </div>*/}
-          {/*  </div>*/}
-          {/*))}*/}
+          {skillsData.map((skill) => (
+            <div key={skill} className="p-2 sm:w-1/2 w-full">
+              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                <CheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                <span className="title-font font-medium text-white">{skill}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
