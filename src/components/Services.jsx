18c7128de70f2dee36dfe5225a034e2a2da96096
arufiand <alfiandannyarmanta@gmail.com>
2023-11-11 23:00:21 +0700

    feat(Screen): Services
    Fixing bad word hahahahaha
    
    Author: @arufiand

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -37,7 +37,7 @@ const Services = () => {
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
         className={`mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]`}>
-        I'm Skilled Yeah Bitch
+        Services that we've covered are
       </motion.p>
 
       <div className={`mt-20 flex flex-wrap gap-10`}>
